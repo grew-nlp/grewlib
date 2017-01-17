grew-nlp/grewlib

commit 01f7b589e2024d7a86fbf8c52dc48d73b6e318d2

change ordering function for features to adapt it to UD sorting
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/semagramme/libcaml-grew/trunk@9142 7838e531-6607-4d57-9587-6c381814729c

src/grew_fs.ml
@@ -25,7 +25,7 @@ module G_feature = struct
 
   let get_name = fst
 
-  let compare feat1 feat2 = Pervasives.compare (get_name feat1) (get_name feat2)
+  let compare feat1 feat2 = Pervasives.compare (String.lowercase_ascii (get_name feat1)) (String.lowercase_ascii (get_name feat2))
 
   (* another order used for printing purpose only *)
   let print_order = ["phon"; "cat"; "lemma"; "pos"]

commit dd2d4df39c948cdc45f7fbf76cfccf837ee65759

Fix a bug introduced in 01f7b589e2024d7a86fbf8c52dc48d73b6e318d2 for handling the feature names ordering
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/semagramme/libcaml-grew/trunk@9197 7838e531-6607-4d57-9587-6c381814729c

VERSION
@@ -1,1 +1,1 @@
-0.40.0
+0.40.1

src/grew_fs.ml
@@ -37,7 +37,7 @@ module G_feature = struct
 
   let get_name = fst
 
-  let compare feat1 feat2 = Pervasives.compare (String.lowercase_ascii (get_name feat1)) (String.lowercase_ascii (get_name feat2))
+  let compare feat1 feat2 = Pervasives.compare (get_name feat1) (get_name feat2)
 
   (* another order used for printing purpose only *)
   let print_order = ["phon"; "cat"; "lemma"; "pos"]
@@ -346,12 +346,18 @@ module G_fs = struct
     let reduced_t = match exclude with
       | None -> t
       | Some list -> List.filter (fun (fn,_) -> not (List.mem fn list || fn.[0]='_')) t in
+    let ud_ordering = (* In UD CoNLL-U format, features are sorted wrt lowercase form *)
+      List.sort
+        (fun feat1 feat2 -> Pervasives.compare (String.lowercase_ascii (G_feature.get_name feat1)) (String.lowercase_ascii (G_feature.get_name feat2)))
+        reduced_t in
     match reduced_t with
       | [] -> "_"
       | _ -> String.concat "|"
         (List.map
-           (function (fn, String "true") -> fn | (fn, fv) -> (decode_feat_name fn)^"="^(string_of_value fv))
-           reduced_t
+          (function 
+            | (fn, String "true") -> fn 
+            | (fn, fv) -> (decode_feat_name fn)^"="^(string_of_value fv))
+          ud_ordering
         )
 end (* module G_fs *)
 