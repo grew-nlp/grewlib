5fe9a4678bb562a5ed32a718e0f105a8af82841b
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2020-03-30 14:04:18 +0200

    add DATADIR handling

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,17 +1,20 @@
 open Ocamlbuild_plugin
- 
-let get_version () =
-  let in_ch = open_in "VERSION" in
-  let v = input_line in_ch in
-  close_in in_ch;
-  v
+
+let read file =
+  let in_ch = open_in file in
+  try
+    let v = input_line in_ch in
+    close_in in_ch;
+    v
+  with End_of_file -> failwith ("Error loading file: "^file)
 
 let () =
   dispatch begin function
   | After_rules ->
-    let version = "\""^(get_version ())^"\"" in
-    let pp_src = S[A"-pp"; A("cppo -D 'VERSION "^version^"'")] in
+    let version = "\""^(read "VERSION")^"\"" in
+    let datadir = "\""^(read "DATADIR")^"\"" in
+    let pp_src = S[A"-pp"; A("cppo -D 'VERSION "^version^"' -D 'DATADIR "^datadir^"'")] in
     flag ["ocaml"; "ocamldep"] & pp_src;
-    flag ["ocaml"; "compile"] & pp_src; 
+    flag ["ocaml"; "compile"] & pp_src;
   | _ -> ()
   end
